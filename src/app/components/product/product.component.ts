b90c7a142598151a288130f481b86801e0f06e74
MyatLinn <you@example.com>
2019-08-06 10:59:26 +0630

    Hello

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from '../../services/product.service';
-import {ActivatedRoute, ParamMap} from '@angular/router';
-import {CartService} from '../../services/cart.service';
-import {Product} from '../../models/product-model';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../models/product-model';
 
 @Component({
   selector: 'app-product',
@@ -11,31 +11,30 @@ import {Product} from '../../models/product-model';
 })
 export class ProductComponent implements OnInit {
 
-    productId: number;
-    product: Product=new Product();
+  productId: number;
+  product: Product = new Product();
+  quantity: number;
 
-    constructor(private service:ProductService,private  route:ActivatedRoute,
-                private cartService:CartService) { }
+  constructor(private service: ProductService, private route: ActivatedRoute,
+    private cartService: CartService) { }
 
-    ngOnInit() {
-      this.route.paramMap.subscribe( (param:ParamMap)=>{
-        this.productId=parseInt(param.get('id'))
-      });
-      this.service.getProduct(this.productId).subscribe((data: Product) => {
-        this.product = data;
-      });
-    }
+  ngOnInit() {
+    this.route.paramMap.subscribe((param: ParamMap) => {
+      this.productId = parseInt(param.get('id'))
+    });
+    this.service.getProduct(this.productId).subscribe((data: Product) => {
+      this.product = data;
+      this.quantity = this.product.quantity;
+    });
+  }
 
-    viewCart() {
+  addToCart(book: any) {
+    this.quantity -= 1;
+    this.cartService.addToCart(book);
+  }
 
-    }
-
-    addToCart(book: any) {
-      this.cartService.addToCart(book);
-    }
-
-    public  getCarts(){
-      return this.cartService.getCarts();
-    }
+  public getCarts() {
+    return this.cartService.getCarts();
+  }
 
 }
